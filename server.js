7a5251e0457a928b7eb31525332168c1fdf6e8cd
rufuscodes <rufuscodes@gmail.com>
2023-05-24 19:32:51 -0700

    feat: Create env variable and add session and flash middleware

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const layouts = require('express-ejs-layouts');
 const app = express();
 
+const session = require('express-session');
+const flash = require('connect-flash');
+
+// Environment variables
+SECRET_SESSION = process.env.SECRET_SESSIONS;
+
 // console.log(SECRET_SESSION);
 
 app.set('view engine', 'ejs');
@@ -11,6 +17,26 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+
+app.use(flash());            // flash middleware
+
+app.use(session({
+  secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
+  resave: false,             // Save the session even if it's modified, make this false
+  saveUninitialized: true    // If we have a new session, we save it, therefore making that true
+}));
+
+
+// add passport 
+
+app.use((req, res, next) => {
+  console.log(res.locals);
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
+
+
 
 app.get('/', (req, res) => {
   res.render('index');
